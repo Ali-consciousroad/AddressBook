49a037f601da58ffd791cda2014d540f253ab105
Ali-consciousroad <ali_dindar@live.be>
2021-03-09 12:03:54 +0100

    connect our app to the database using mongoose

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express')//Initialize express app
 const app = express();//Initialize the sever
+const mongoose = require('mongoose')// Require mongoose in our app
 app.listen(3000, () => {
     console.log('sever listening on port:3000');
 });
+
+// Connecting to DB
+mongoose.connect('mongodb://localhost:27017/AddressBook', {useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => {
+ console.log('connected to db')
+}).catch((error) => {
+ console.log(error)
+})
