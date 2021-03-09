d14dbb162c59f6a2b5408e7c40048c056347ed06
Ali-consciousroad <ali_dindar@live.be>
2021-03-09 15:37:03 +0100

    adding a user details

--- index.js
+++ index.js
@@ -14,3 +14,24 @@ mongoose.connect('mongodb://localhost:27017/AddressBook', {useNewUrlParser: true
 }).catch((error) => {
  console.log(error)
 })
+
+// Adding a User to AddressBook
+app.post('/', (req, res) => {
+ name = req.body.name,
+ email = req.body.email,
+ phone = req.body.phone,
+ place = req.body.placelet
+
+ let newAddress = new Address({
+  name: name,
+  email: email,
+  phone: phone,
+  place: place
+ })
+
+ newAddress.save().then((address) => {
+  res.send(address)
+ }).catch((err) => {
+  console.log(error)
+ })
+})
